251c1cedd41c7e51e26cf3dc669fe9e2e9a5ef4a
kinolollipop <lovezax0213@gmail.com>
2019-09-11 10:32:20 +0800

    zip dist

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Homeeeee</router-link> |
+      <router-link to="/">Homeee</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
